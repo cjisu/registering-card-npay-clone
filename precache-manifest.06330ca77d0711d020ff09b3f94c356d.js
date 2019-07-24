8939959f15f649d334e22b1c98f84349ac239660
CHOI jisu <h41469921@gmail.com>
2019-07-25 00:22:24 +0900

    Updates

---
+++ precache-manifest.06330ca77d0711d020ff09b3f94c356d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d79bdf90c6a6a751253991c257c2d5",
+    "url": "/registering-card-npay-clone/index.html"
+  },
+  {
+    "revision": "e98126264dd076497ce3",
+    "url": "/registering-card-npay-clone/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/registering-card-npay-clone/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "e98126264dd076497ce3",
+    "url": "/registering-card-npay-clone/static/js/main.7782bbf2.chunk.js"
+  },
+  {
+    "revision": "4f7ce8b38c9ee176a561",
+    "url": "/registering-card-npay-clone/static/js/runtime~main.da45958b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/registering-card-npay-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
