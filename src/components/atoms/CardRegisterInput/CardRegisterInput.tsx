5519d30d0103602993ef324804bbf15264eb8d20
CHOI jisu <h41469921@gmail.com>
2019-07-29 23:23:35 +0900

    Create CardRegisterInput atom component

---
+++ src/components/atoms/CardRegisterInput/CardRegisterInput.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 60px;
+  height: 28px;
+  font-size: 20px;
+  border: none;
+  color: ${p => p.theme.colors.inputColor};
+`;
