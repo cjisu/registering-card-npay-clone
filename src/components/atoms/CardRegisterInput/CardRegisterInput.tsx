9a2a87be2607b8b4adebec042210db9eb17ccfa5
CHOI jisu <h41469921@gmail.com>
2019-08-01 00:08:39 +0900

    Change font size

--- src/components/atoms/CardRegisterInput/CardRegisterInput.tsx
+++ src/components/atoms/CardRegisterInput/CardRegisterInput.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export default styled.input<{ inputWidth?: number }>`
-  width: ${p => (p.inputWidth ? p.inputWidth + 'px' : '95%')};
+  width: ${p => (p.inputWidth ? p.inputWidth + 'px' : '90%')};
   height: 28px;
-  font-size: 17px;
+  font-size: 0.9em;
   border: none;
   color: ${p => p.theme.colors.inputColor};
 `;
